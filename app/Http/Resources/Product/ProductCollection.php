cc3d6190fac1b84d2b1b799d4f1e7679d91ead55
Augustine Afriyie <augustineafriyie1@gmail.com>
2020-02-19 16:32:39 +0000

    First Commit

---
+++ app/Http/Resources/Product/ProductCollection.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\Product;
+
+use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends Resource
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+       // return parent::toArray($request);
+
+        return [
+            'name'=>$this->name,
+
+            'totalprice' =>$this->price,
+
+            'rating' => $this->reviews->count()> 0 ?  round($this->reviews->sum('star')/$this->reviews->count(),2) :'No Rating',
+            'href' => [
+                'link' => route('products.show',$this->id)
+            ]
+        ];
+    }
+}
